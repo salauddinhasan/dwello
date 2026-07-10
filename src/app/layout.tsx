46fc1e79b94d58a37917b350c54e4d5ac96729f0
salauddin <salauddin@hasan.com>
2026-07-10 17:40:51 +0600

    Add Footer component with SVG social icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Navbar />
         <main className="flex-1">{children}</main>
+        <Footer/>
       </body>
     </html>
   );
